c00649319d73d754b591b3622b78126225bfa3d6
Todd Yamakawa <todd.yamakawa@arm.com>
2017-04-21 09:14:16 -0500

    tmux: Temporarily removing bjobs from status bar.

--- bjobs.zsh
+++ bjobs.zsh
@@ -1,4 +1,6 @@
 #!/bin/zsh
+local here=${0:h}
+which bjobs &> /dev/null || exit
 
 red="#[fg=colour9]"
 yellow="#[fg=colour11]"
@@ -7,15 +9,13 @@ blue="#[fg=colour33]"
 white="#[fg=colour15]"
 comma="$blue,"
 
-jobs=$(bjobs -noheader -a -o stat)
-done=$white$(echo $jobs | grep -c DONE)
-exit=$red$(echo $jobs | grep -c EXIT)
-pend=$yellow$(echo $jobs | grep -c PEND)
-run=$green$(echo $jobs | grep -c RUN)
+#jobs=$(bjobs -noheader -a -o stat)
+jobs=$(cat $here/bjobs.txt)
 
-string=$run$comma
-string+=$pend$comma
-string+=$done$comma
-string+=$exit$blue
+string=$blue'['
+string+=$green$( echo $jobs | grep -c RUN )$comma
+string+=$yellow$(echo $jobs | grep -c PEND)$comma
+string+=$white$( echo $jobs | grep -c DONE)$comma
+string+=$red$(   echo $jobs | grep -c EXIT)$blue']'
 echo $string
 
