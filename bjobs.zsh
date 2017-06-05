0737718765275909832c644a7fa83ce84bfa452c
Todd Yamakawa <todd.yamakawa@arm.com>
2017-06-05 12:13:44 -0500

    tmux: Updating bjobs script.

--- bjobs.zsh
+++ bjobs.zsh
@@ -1,9 +1,19 @@
 #!/bin/zsh
 local here=${0:h}
-which bjobs &> /dev/null || exit
+which bjobs &> /dev/null || return
 
+# Generate bjobs.txt
+[[ -f $here/bjobs.txt ]] || touch $here/bjobs.txt
+age=$(($(date +%s)-$(stat -c %Y $here/bjobs.txt)))
+[[ $age -lt 300 ]] && bjobs -noheader -a -o stat > $here/bjobs.txt
+
+# Get job information from bjobs.txt
+#[[ -s $here/bjobs.txt ]] || return
+grep "No job found" $here/bjobs.txt && return
+jobs=$(cat $here/bjobs.txt)
+
+# tmux colors and characters
 source $here/colors.conf
-
 red="#[fg=$tmux_red]"
 yellow="#[fg=$tmux_yellow]"
 green="#[fg=$tmux_green]"
@@ -11,9 +21,7 @@ blue="#[fg=$tmux_blue]"
 white="#[fg=$tmux_white]"
 comma="$blue,"
 
-#jobs=$(bjobs -noheader -a -o stat)
-jobs=$(cat $here/bjobs.txt)
-
+# Print tmux job string
 string=$blue'['
 string+=$green$( echo $jobs | grep -c RUN )$comma
 string+=$yellow$(echo $jobs | grep -c PEND)$comma
