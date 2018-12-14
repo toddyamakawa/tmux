cfcb62ead9ef7234acb2cd9681b396a3eb7bb36a
Todd Yamakawa <todd.yamakawa@arm.com>
2018-12-14 14:47:38 -0600

    tmux: Adding check for killing panes.

---
+++ bin/kill-pane.bash
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+declare -r cmd=$(tmux display-message -p '#{pane_current_command}')
+
+case "$cmd" in
+
+	# Keep pane alive if running LSF job
+	nios)
+		tmux display-message "Unable to kill pane"
+		;;
+
+	# Confirm before killing pane
+	vim|ssh)
+		tmux confirm-before -p "kill-pane running $cmd? (y/n)" kill-pane
+		;;
+
+	# Kill pane running zsh
+	zsh)
+		tmux kill-pane
+		;;
+
+	*)
+		tmux confirm-before -p "kill-pane running $cmd? (y/n)" kill-pane
+		;;
+esac
+
