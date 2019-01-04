837a3904a45adbec3dd42398b0a57fef1d76938c
Todd Yamakawa <todd.yamakawa@arm.com>
2019-01-04 11:42:32 -0600

    tmux/theme: Adding tmux version and memory usage.

---
+++ bin/mem-usage.zsh
@@ -0,0 +1,11 @@
+#!/usr/bin/env zsh
+awk '
+	/^MemTotal:/     { total=$2 }
+	/^MemFree:/      { free=$2 }
+	/^Buffers:/      { buffers=$2 }
+	/^Cached:/       { cached=$2 }
+	/^Shmem:/        { shmem=$2 }
+	/^SReclaimable:/ { srec=$2 }
+	END { printf "%0.1f/%d GB", (total-free-buffers-cached+shmem-srec)/1048576, total/1048576 }
+' /proc/meminfo
+
