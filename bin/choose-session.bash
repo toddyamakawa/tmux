efa99fe7146c9507bfe1919a5dda695f841a74c1
Todd Yamakawa <todd.yamakawa@arm.com>
2020-01-27 10:37:21 -0600

    bin: Moving scripts into tmux-prefixless plugin.

--- bin/choose-session.bash
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-tmux list-sessions -F "#{session_id} '#{session_name}'" \
-	| awk '{ cmd="'"'"'switch-client -t "$1"'"'"'"
-		key="'"'"'"counter++"'"'"'"
-		$1=""
-		print $0, key, cmd
-	}' \
-	| xargs tmux display-menu -y S
-
--- bin/copy-pane-path.bash
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-tmux display-message -p "#{pane_current_path}" | xclip
